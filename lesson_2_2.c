7e85b58a2ad90540b1b7a8e4531d333304c05dbb
Gulyás Simone <gulyassimone@gmail.com>
2019-11-10 09:53:47 +0100

    remove

--- lesson_2_2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "lesson_2_2.h"
-
-int main()
-{
-double c=fahrcels(25);
-return 0;
-}
-
--- lesson_2_2_f.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-double fahrcels(double x)
-{
-	return 5./9.*(x-32);
-}
